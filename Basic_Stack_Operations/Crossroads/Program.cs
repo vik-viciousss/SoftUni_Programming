a82ab6e0c80e7e0a6ab77537783f49038ac052a4
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-04-03 20:42:20 +0300

    Stack-Operations

---
+++ Basic_Stack_Operations/Crossroads/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secondsGreen = int.Parse(Console.ReadLine());
+            int freeWindowDuration = int.Parse(Console.ReadLine());
+            int timeLeftGreen = secondsGreen;
+            int carsPassed = 0;
+            bool hasOccured = false;
+
+            string command = Console.ReadLine();
+
+            Queue<string> cars = new Queue<string>();
+
+            while (command != "END")
+            {
+                if (command != "green")
+                {
+                    cars.Enqueue(command);
+                }
+                else
+                {
+                    string currCar = cars.Peek();
+
+                    if (currCar.Length <= timeLeftGreen)
+                    {
+                        cars.Dequeue();
+                        carsPassed++;
+                        timeLeftGreen -= currCar.Length;
+
+                        if (cars.Count < 1)
+                        {
+                            command = Console.ReadLine();
+                            timeLeftGreen = secondsGreen;
+                        }
+                        continue;
+                    }
+                    else
+                    {
+                        if (currCar.Length - timeLeftGreen <= freeWindowDuration)
+                        {
+                            cars.Dequeue();
+                            carsPassed++;
+                        }
+                        else
+                        {
+                            int charsLeft = currCar.Length - timeLeftGreen - freeWindowDuration - 1;
+                            int charIndex = currCar.Length - charsLeft - 1;
+
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{currCar} was hit at {currCar[charIndex]}.");
+                            hasOccured = true;
+                            break;
+                        }
+                    }
+                }
+
+                timeLeftGreen = secondsGreen;
+                command = Console.ReadLine();
+            }
+
+            if (!hasOccured)
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{carsPassed} total cars passed the crossroads.");
+            }
+        }
+    }
+}
---
+++ Basic_Stack_Operations/Simple_Text_Editor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Stack<string> changes = new Stack<string>();
+            Stack<string> lastCommands = new Stack<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command[0] == "1")
+                {
+                    string textToAdd = command[1];
+                    sb.Append(textToAdd);
+                    changes.Push(textToAdd);
+                    lastCommands.Push(command[0]);
+                }
+                else if (command[0] == "2")
+                {
+                    int count = int.Parse(command[1]);
+                    int startIndex = sb.Length - count;
+
+                    string removed = sb.ToString().Substring(startIndex);
+
+                    sb.Remove(startIndex, count); 
+
+                    changes.Push(removed.ToString());
+                    lastCommands.Push(command[0]);
+                }
+                else if (command[0] == "3")
+                {
+                    int charCounter = int.Parse(command[1]);
+                    Console.WriteLine(sb[charCounter - 1]);
+                }
+                else if (command[0] == "4")
+                {
+                    if (lastCommands.Peek() == "1")
+                    {
+                        lastCommands.Pop();
+                        string textToRemove = changes.Pop();
+                        int startIndex = sb.Length - textToRemove.Length;
+                        sb.Remove(startIndex, textToRemove.Length);
+                    }
+                    else if (lastCommands.Peek() == "2")
+                    {
+                        lastCommands.Pop();
+                        string textToAddBack = changes.Pop();
+                        sb.Append(textToAddBack);
+                    }
+                }
+            }
+        }
+    }
+}
