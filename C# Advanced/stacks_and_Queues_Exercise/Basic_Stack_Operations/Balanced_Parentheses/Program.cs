0c64dfa8193e1f1fabfbb3beef1069e703bb47ce
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-04-18 14:42:49 +0300

    exercises

---
+++ C# Advanced/stacks_and_Queues_Exercise/Basic_Stack_Operations/Balanced_Parentheses/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> open = new Stack<char>();
+
+            string input = Console.ReadLine();
+
+            foreach (char item in input)
+            {
+                if (item == '(' || item == '[' || item == '{')
+                {
+                    open.Push(item);
+                }
+                else
+                {
+                    if (item == ')' && open.Peek() == '(')
+                    {
+                        open.Pop();
+                    }
+                    else if (item == ']' && open.Peek() == '[')
+                    {
+                        open.Pop();
+                    }
+                    else if (item == '}' && open.Peek() == '{')
+                    {
+                        open.Pop();
+                    }
+                }
+            }
+
+            if (open.Any())
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+
+        }
+    }
+}
---
+++ C# Advanced/stacks_and_Queues_Exercise/Basic_Stack_Operations/KeyRevolver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int gunBarrelSize = int.Parse(Console.ReadLine());
+
+            int[] bulletsInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> bullets = new Stack<int>(bulletsInput);
+
+            int[] locksInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> locks = new Queue<int>(locksInput);
+
+            int intelligenceValue = int.Parse(Console.ReadLine());
+
+            while (locks.Any() && bullets.Any())
+            {
+                int currBullet = bullets.Peek();
+                int currLock = locks.Dequeue();
+
+
+            }
+
+        }
+    }
+}
