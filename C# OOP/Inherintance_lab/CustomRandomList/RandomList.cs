71dcb25cc6bb509215e6406eb9112984e97a60e3
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-05-11 15:07:34 +0300

    inheritance

---
+++ C# OOP/Inherintance_lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public void Add(string element)
+        {
+            base.Add(element);
+
+        }
+
+        public string RandomString()
+        {
+            return this[random.Next(0, this.Count)];
+        }
+    }
+}
---
+++ C# OOP/Inherintance_lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (base.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+
+            return this;
+        }
+    }
+}
