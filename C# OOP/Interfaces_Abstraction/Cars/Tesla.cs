ecf4b94e8d0d1c47c92dc5e125b4b38d13639ff8
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-05-27 16:04:15 +0300

    more exercises

---
+++ C# OOP/Interfaces_Abstraction/Cars/Tesla.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        private string model;
+        private string color;
+        private int batteries;
+
+        public Tesla(string model, string color, int batteries)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Batteries = batteries;
+        }
+
+        public int Batteries
+        {
+            get => this.batteries;
+            private set
+            {
+                this.batteries = value;
+            }
+        }
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                this.model = value;
+            }
+        }
+        public string Color
+        {
+            get => this.color;
+            private set
+            {
+                this.color = value;
+            }
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Batteries} Batteries";
+        }
+    }
+}
