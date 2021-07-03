c326c699781e3fc0671db92a4243a10bf000c35b
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-07-03 23:54:05 +0300

    Revert "football_team_generator_optimization"
    This reverts commit 87e3f152168a4c0fc69e7eb843d5fb2c91d6db3d.

--- C# OOP/Encapsulation_Exercise/FootballTeamGenerator/StartUp.cs
+++ C# OOP/Encapsulation_Exercise/FootballTeamGenerator/StartUp.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace FootballTeamGenerator
 {
@@ -8,200 +7,86 @@ namespace FootballTeamGenerator
     {
         static void Main(string[] args)
         {
-            List<Team> teams = new List<Team>();
+            var cmdArgs = Console.ReadLine().Split(";");
 
-            string input = string.Empty;
+            var teamsByName = new Dictionary<string, Team>();
 
-            while ((input = Console.ReadLine()) != "END")
+            while (cmdArgs[0] != "END")
             {
-                string[] tokens = input.Split(';', StringSplitOptions.RemoveEmptyEntries);
-
-                string command = tokens[0];
-                string teamName = tokens[1];
-                try
+                if (cmdArgs[0] == "Team")
                 {
-                    switch (command)
+                    var teamName = cmdArgs[1];
+
+                    try
                     {
-                        case "Team":
-                            try
-                            {
-                                Team newTeam = new Team(teamName);
-                                teams.Add(newTeam);
-                            }
-                            catch (ArgumentException e)
-                            {
-                                Console.WriteLine(e.Message);
-                            }
-                            break;
-                        case "Add":
-
-                            Team current = teams.Where(x => x.Name == teamName).FirstOrDefault();
-
-                            if (current == null)
-                            {
-                                Console.WriteLine($"Team {teamName} does not exist.");
-                            }
-                            else
-                            {
-                                string playerName = tokens[2];
-                                int endurance = int.Parse(tokens[3]);
-                                int sprint = int.Parse(tokens[4]);
-                                int dribble = int.Parse(tokens[5]);
-                                int passing = int.Parse(tokens[6]);
-                                int shooting = int.Parse(tokens[7]);
-
-                                try
-                                {
-                                    Player player = new Player(playerName, endurance, sprint, dribble, passing, shooting);
-                                    current.AddPlayer(player);
-                                }
-                                catch (ArgumentException e)
-                                {
-                                    Console.WriteLine(e.Message);
-                                }
-
-                            }
-                            break;
-                        case "Remove":
-
-                            string playerToRemove = tokens[2];
-
-                            Team currentTeam = teams.Where(x => x.Name == teamName).First();
-
-                            try
-                            {
-                                currentTeam.RemovePlayer(playerToRemove);
-                            }
-                            catch (ArgumentException e)
-                            {
-                                Console.WriteLine(e.Message);
-                            }
-                            break;
-                        case "Rating":
-
-                            Team team = teams.FirstOrDefault(x => x.Name == teamName);
-
-                            if (team == null)
-                            {
-                                Console.WriteLine($"Team {teamName} does not exist.");
-                            }
-
-                            Console.WriteLine($"{teamName} - {team.AverageRating}");
-                            break;
+                        var team = new Team(teamName);
+                        teamsByName.Add(teamName, team);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
                     }
                 }
-                catch (Exception)
+                else if (cmdArgs[0] == "Add")
                 {
+                    var teamName = cmdArgs[1];
+
+                    if (!teamsByName.ContainsKey(teamName))
+                    {
+                        Console.WriteLine($"Team {teamName} does not exist.");
+                        continue;
+                    }
+
+                    var playerName = cmdArgs[2];
+                    var endurance = int.Parse(cmdArgs[3]);
+                    var sprint = int.Parse(cmdArgs[4]);
+                    var dribble = int.Parse(cmdArgs[5]);
+                    var passing = int.Parse(cmdArgs[6]);
+                    var shooting = int.Parse(cmdArgs[7]);
+
+                    try
+                    {
+                        var player = new Player(playerName, endurance, sprint, dribble, passing, shooting);
+
+                        teamsByName[teamName].AddPlayer(player);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
 
                 }
+                else if (cmdArgs[0] == "Remove")
+                {
+                    var teamName = cmdArgs[1];
+                    var playerName = cmdArgs[2];
+
+                    try
+                    {
+                        teamsByName[teamName].RemovePlayer(playerName);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (cmdArgs[0] == "Rating")
+                {
+                    var teamName = cmdArgs[1];
+
+                    if (!teamsByName.ContainsKey(teamName))
+                    {
+                        Console.WriteLine($"Team {teamName} does not exist.");
+                        continue;
+                    }
+
+                    var team = teamsByName[teamName];
+
+                    Console.WriteLine($"{teamName} - {team.AverageRating}");
+                }
+
+                cmdArgs = Console.ReadLine().Split(";");
             }
-
-
-
-            //var cmdArgs = Console.ReadLine().Split(";");
-
-            ////Dictionary<string, Team> teamsByName = new Dictionary<string, Team>();
-
-            //List<Team> teams = new List<Team>();
-
-            //while (cmdArgs[0] != "END")
-            //{
-            //    string command = cmdArgs[0];
-            //    string teamName = cmdArgs[1];
-
-            //    try
-            //    {
-            //        if (command == "Team")
-            //        {
-            //            try
-            //            {
-            //                Team team = new Team(teamName);
-            //                teams.Add(team);
-            //            }
-            //            catch (Exception ex)
-            //            {
-            //                Console.WriteLine(ex.Message);
-            //            }
-            //        }
-            //        else if (command == "Add")
-            //        {
-            //            //if (!teamsByName.ContainsKey(teamName))
-            //            //{
-            //            //    Console.WriteLine($"Team {teamName} does not exist.");
-            //            //    continue;
-            //            //}
-
-            //            Team current = teams.Where(x => x.Name == teamName).FirstOrDefault();
-
-            //            if (current == null)
-            //            {
-            //                Console.WriteLine($"Team {teamName} does not exist.");
-            //                continue;
-            //            }
-
-            //            string playerName = cmdArgs[2];
-            //            int endurance = int.Parse(cmdArgs[3]);
-            //            int sprint = int.Parse(cmdArgs[4]);
-            //            int dribble = int.Parse(cmdArgs[5]);
-            //            int passing = int.Parse(cmdArgs[6]);
-            //            int shooting = int.Parse(cmdArgs[7]);
-
-            //            try
-            //            {
-            //                Player player = new Player(playerName, endurance, sprint, dribble, passing, shooting);
-
-            //                current.AddPlayer(player);
-            //            }
-            //            catch (Exception ex)
-            //            {
-            //                Console.WriteLine(ex.Message);
-            //            }
-
-            //        }
-            //        else if (command == "Remove")
-            //        {
-            //            var playerName = cmdArgs[2];
-
-            //            Team currentTeam = teams.Where(x => x.Name == teamName).First();
-
-            //            try
-            //            {
-            //                currentTeam.RemovePlayer(playerName);
-            //            }
-            //            catch (Exception ex)
-            //            {
-            //                Console.WriteLine(ex.Message);
-            //            }
-            //        }
-            //        else if (command == "Rating")
-            //        {
-            //            //if (!teamsByName.ContainsKey(teamName))
-            //            //{
-            //            //    Console.WriteLine($"Team {teamName} does not exist.");
-            //            //    continue;
-            //            //}
-
-            //            //var team = teamsByName[teamName];
-
-            //            //Team team = teamsByName.Values.ToList().FirstOrDefault(x => x.Name == teamName);
-            //            Team team = teams.FirstOrDefault(x => x.Name == teamName);
-
-            //            if (team == null)
-            //            {
-            //                Console.WriteLine($"Team {teamName} does not exist.");
-            //                continue;
-            //            }
-
-            //            Console.WriteLine($"{teamName} - {team.AverageRating}");
-            //        }
-            //    }
-            //    catch (Exception)
-            //    {
-
-            //    }
-
-            //    cmdArgs = Console.ReadLine().Split(";");
-            //}
         }
     }
 }
