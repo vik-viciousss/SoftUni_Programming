06ee35ba2ba6e1d731417273320a94c5f69f0379
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-07-15 20:02:46 +0300

    command_pattern

---
+++ C# OOP/ReflectionAndAttributes_Exercise/CommandPattern/Core/CommandFactory.cs
@@ -0,0 +1,29 @@
+﻿using CommandPattern.Commands;
+using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandFactory : ICommandFactory
+    {
+        public ICommand CreateCommand(string commandType)
+        {
+            //Assembly.GetAssembly(typeof(StartUp));
+
+            Type type = Assembly.GetEntryAssembly().GetTypes().FirstOrDefault(t => t.Name.StartsWith(commandType));
+
+            if (type == null)
+            {
+                throw new ArgumentException($"{commandType} is invalid command type.");
+            }
+
+            var instance = (ICommand)Activator.CreateInstance(type);
+
+            return instance;
+        }
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes_Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,26 @@
+﻿using CommandPattern.Commands;
+using CommandPattern.Core.Contracts;
+using System.Linq;
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private readonly ICommandFactory commandFactory;
+
+        public CommandInterpreter()
+        {
+            this.commandFactory = new CommandFactory();
+        }
+
+        public string Read(string args)
+        {
+            string[] parts = args.Split();
+            string commandType = parts[0];
+            string[] commandArgs = parts.Skip(1).ToArray();
+
+            ICommand command = this.commandFactory.CreateCommand(commandType);
+
+            return command.Execute(commandArgs);
+        }
+    }
+}
