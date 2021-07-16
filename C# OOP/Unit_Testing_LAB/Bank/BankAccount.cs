a8a9435f38d19de8d941be258c97c4e6c0a48f88
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-07-16 20:44:10 +0300

    unit_testing_basics

---
+++ C# OOP/Unit_Testing_LAB/Bank/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class BankAccount 
+    {
+        private decimal amount = 0;
+
+        public BankAccount(decimal amount, string name)
+        {
+            this.Amount = amount;
+            this.Name = name;
+        }
+
+        public decimal Amount
+        {
+            get => this.amount;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("No negatives");
+                }
+
+                this.amount = value;
+            }
+        }
+
+        public string Name { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Amount += amount;
+        }
+    }
+}
