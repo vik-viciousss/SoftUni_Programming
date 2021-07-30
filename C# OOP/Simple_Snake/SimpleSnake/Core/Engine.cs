401034176f415a8c9f11f39709d2ea17aa473172
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-07-30 19:01:45 +0300

    game objects created

---
+++ C# OOP/Simple_Snake/SimpleSnake/Core/Engine.cs
@@ -0,0 +1,31 @@
+﻿using SimpleSnake.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSnake.Core
+{
+    public class Engine
+    {
+        private Point[] pointsOfDirection;
+
+        public void Run()
+        {
+
+        }
+
+        private void CreateDirections()
+        {
+            this.pointsOfDirection[0] = new Point(1, 0);
+            this.pointsOfDirection[1] = new Point(-1, 0);
+            this.pointsOfDirection[2] = new Point(0, 1);
+            this.pointsOfDirection[3] = new Point(0, -1);
+        }
+
+        private void GetNextDirection()
+        {
+
+        }
+
+    }
+}
---
+++ C# OOP/Simple_Snake/SimpleSnake/GameObjects/Snake.cs
@@ -0,0 +1,101 @@
+﻿using SimpleSnake.GameObjects.FoodTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Snake
+    {
+        private Queue<Point> snakeElements;
+        private Food[] food;
+        private Wall wall;
+        private int nextLeftX;
+        private int nextTopY;
+        private const char snakeSymbol = '\u22CF';
+        private int foodIndex;
+
+        public Snake(Wall wall)
+        {
+            this.wall = wall;
+            this.snakeElements = new Queue<Point>();
+            this.food = new Food[3];
+            this.foodIndex = RandomFoodNumber;
+            this.GetFoods();
+            this.CreateSnake();
+        }
+
+        private void CreateSnake()
+        {
+            for (int topY = 1; topY <= 6; topY++)
+            {
+                this.snakeElements.Enqueue(new Point(2, topY));
+            }
+        }
+
+        private void GetFoods()
+        {
+            this.food[0] = new FoodHash(this.wall);
+            this.food[1] = new FoodDollar(this.wall);
+            this.food[2] = new FoodAsterisk(this.wall);
+        }
+
+        private bool IsMoving(Point direction)
+        {
+            Point currentSnakeHead = this.snakeElements.Last();
+
+            this.GetNextPoint(direction, currentSnakeHead);
+
+            bool IsPointOfSnake = this.snakeElements.Any(x => x.LeftX == nextLeftX && x.TopY == nextLeftX);
+
+            if (IsPointOfSnake)
+            {
+                return false;
+            }
+
+            Point snakeNewHead = new Point(this.nextLeftX, this.nextTopY);
+
+            if (this.wall.IsPointOfWall(snakeNewHead))
+            {
+                return false;
+            }
+
+
+            //////////////////////////////////////////
+            this.snakeElements.Enqueue(snakeNewHead);
+            snakeNewHead.Draw(snakeSymbol);
+
+            if (food[foodIndex].IsFoodPoint(snakeNewHead))
+            {
+                this.Eat(direction, currentSnakeHead);
+            }
+
+            Point snakeTail = this.snakeElements.Dequeue();
+            snakeTail.Draw(' ');
+
+            return true;
+        }
+
+        private void Eat(Point direction, Point currentSnakeHead)
+        {
+            int length = food[foodIndex].FoodPoints;
+
+            for (int i = 0; i < length; i++)
+            {
+                this.snakeElements.Enqueue(new Point(this.nextLeftX, this.nextTopY));
+                GetNextPoint(direction, currentSnakeHead);
+            }
+
+            this.foodIndex = this.RandomFoodNumber;
+            this.food[foodIndex].SetRandomPosition(this.snakeElements);
+        }
+
+        private void GetNextPoint(Point direction, Point snakeHead)
+        {
+            this.nextLeftX = snakeHead.LeftX + direction.LeftX;
+            this.nextTopY = snakeHead.TopY + direction.TopY;
+        }
+
+        private int RandomFoodNumber => new Random().Next(0, this.food.Length);
+    }
+}
