7454723aae28a708dc7a09a552b67428631e3277
Viktoria Marinova <64691525+vik-viciousss@users.noreply.github.com>
2021-08-03 19:49:57 +0300

    reversed_list_implementation

---
+++ Data_Structures_Fundamentals/Linear_Data_Structures/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,54 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                if (parentheses[i] == '{' || parentheses[i] == '[' || parentheses[i] == '(')
+                {
+                    stack.Push(parentheses[i]);
+                }
+                else if (parentheses[i] == '}' || parentheses[i] == ']' || parentheses[i] == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    if (parentheses[i] == '}')
+                    {
+                        if (stack.Peek() != '{')
+                        {
+                            return false;
+                        }
+                        stack.Pop();
+                    }
+                    else if (parentheses[i] == ']')
+                    {
+                        if (stack.Peek() != '[')
+                        {
+                            return false;
+                        }
+                        stack.Pop();
+                    }
+                    else if (parentheses[i] == ')')
+                    {
+                        if (stack.Peek() != '(')
+                        {
+                            return false;
+                        }
+                        stack.Pop();
+                    }
+                }
+            }
+            
+            return stack.Count == 0;
+        }
+    }
+}
